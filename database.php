7ae99a2d3c9fc3d4df281510310185edb66f9aa8
Demura AI <71892583+DemuraAIdev@users.noreply.github.com>
2022-06-03 12:55:36 +0000

    Update Utama

--- database.php
+++ database.php
@@ -3,4 +3,17 @@ $host="senkoprojects.xyz";
 $user="root";
 $password="";
 $db="database"
+
+$kon = mysqli_connect($host,$user,$password);
+if ($kon){
+	echo "Connection succses";
+  } else {
+  echo "Connection missing"
+  }
+$sellect=mysqli_select_db($kon,$db);
+if ($sellect) {
+  echo "Sellected Database $db "
+} else {
+  echo "Something Worng"
+}
 ?>
